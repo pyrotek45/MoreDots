b3b99d011c4fddeaf94aa9a30d6a20184af597f8
Vincent Ragusa <vincent.ragusa.94@gmail.com>
2025-02-07 11:28:59 -0500

    added unit stats from old version and added game reset function

--- dots2.py
+++ dots2.py
@@ -81,24 +81,80 @@ class Unit(pygame.sprite.Sprite):
 # Unit Definitions #
 
 # Mele Unit
-mele_shape = {"color":0, "radius":0}
-mele_stats = {"max_health":0, "atk":0, "def":0, "speed":0, "vision":0}
+mele_shape = {"radius":20}
+mele_stats = {"max_health":100, "atk":10, "def":5, "speed":5, "vision":100}
 
 # Tank Unit
-tank_shape = {"color":0, "radius":0}
-tank_stats = {"max_health":0, "atk":0, "def":0, "speed":0, "vision":0}
+tank_shape = {"radius":30}
+tank_stats = {"max_health":200, "atk":20, "def":10, "speed":3, "vision":100}
 
 # Rush Unit
-rush_shape = {"color":0, "radius":0}
-rush_stats = {"max_health":0, "atk":0, "def":0, "speed":0, "vision":0}
+rush_shape = {"radius":5}
+rush_stats = {"max_health":50, "atk":40, "def":2, "speed":8, "vision":100}
+
+##################################################################################################
+def get_random_y_pos(screen_height):
+    return screen_height*random.random()
+
+
+def reset_game_state(screen_height, screen_width):
+    """resets the game state (total erasure of all data)"""
+    player_army = pygame.sprite.Group()
+    enemy_army = pygame.sprite.Group()
+
+    num_mele = 10
+    num_tank = 5
+    num_rush = 15
+
+    for _ in range(num_mele):
+        player_army.add(Unit("Mele",
+                             pygame.Vector2(0, get_random_y_pos(screen_height)),
+                             mele_shape | {"color": "blue"},
+                             mele_stats,
+                             random.randint(500)))
+        enemy_army.add(Unit("Mele",
+                             pygame.Vector2(screen_width, get_random_y_pos(screen_height)),
+                             mele_shape | {"color": "red"},
+                             mele_stats,
+                             random.randint(500)))
+        
+    for _ in range(num_tank):
+        player_army.add(Unit("Tank",
+                             pygame.Vector2(0, get_random_y_pos(screen_height)),
+                             tank_shape | {"color": "blue"},
+                             tank_stats,
+                             random.randint(500)))
+        enemy_army.add(Unit("Tank",
+                             pygame.Vector2(screen_width, get_random_y_pos(screen_height)),
+                             tank_shape | {"color": "red"},
+                             tank_stats,
+                             random.randint(500)))
+        
+    for _ in range(num_rush):
+        player_army.add(Unit("Rush",
+                             pygame.Vector2(0, get_random_y_pos(screen_height)),
+                             rush_shape | {"color": "blue"},
+                             rush_stats,
+                             random.randint(500)))
+        enemy_army.add(Unit("Mele",
+                             pygame.Vector2(screen_width, get_random_y_pos(screen_height)),
+                             rush_shape | {"color": "red"},
+                             rush_stats,
+                             random.randint(500)))
+
+    return player_army, enemy_army
+
+
 
 ##################################################################################################
 
 # MAIN (all runtime code goes in here)
 if __name__ == "__main__":
     pygame.init()
-    test_screen = pygame.display.set_mode((1280, 720))
-    test_pos = pygame.Vector2(test_screen.get_width() / 2, test_screen.get_height() / 2)
-    test_spawn_time = random.randint(0,500)
 
-    test_unit = Unit("test_type", test_pos, mele_shape, mele_stats, test_spawn_time)
+    # Global Variables for game state
+    screen = pygame.display.set_mode((1280, 720))
+    goal_left = pygame.Vector2(0, screen.get_height() / 2)
+    goal_right = pygame.Vector2(screen.get_width(), screen.get_height() / 2)
+
+    player_army, enemy_army = reset_game_state(screen.get_height(), screen.get_width())
